f877d50a9f384bdd1555123580d184ef6680d421
Abdul Ghodir <abdulghodir08@gmail.com>
2025-09-02 13:33:55 +0700

    test

---
+++ tes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Masukkan jumlah bilangan Fibonacci: ";
+    cin >> n;
+
+    int a = 0, b = 1, c;
+
+    cout << "Deret Fibonacci: ";
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    cout << endl;
+    return 0;
+}
